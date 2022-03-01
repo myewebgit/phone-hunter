96c681b30e8d9eb47ef4099a65152e1af8371739
myewebgit <mnislamctg7@gmail.com>
2022-03-01 23:30:10 +0600

    second commit

--- phone.js
+++ phone.js
@@ -17,13 +17,68 @@ const displaySearchResult = data =>{
         console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML = `<div class="card h-100">
+        div.innerHTML = `
+        <div onclick="loadPhoneDetail('${phone.slug}')" class="card h-25">
         <img src="${phone.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="card-body rounded-3">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">${phone.brand}</p>
+          <a href="${div.innerHTML}" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
       </div>`;
       searchResult.appendChild(div);
     })
 }
+
+const loadPhoneDetail = (id) =>{
+    // const phoneId = phone.slug.value;
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetail(data.data));
+    // console.log(phoneId);
+}
+const displayPhoneDetail = datap =>{
+    console.log(datap);
+    const phoneDetail = document.getElementById('phone-detail');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${datap.image}" class="card-img-top w-100" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${datap.brand}</h5>
+            <h5 class="card-title">${datap.name}</h5>
+            <button id='info-field' onclick="info('${datap.slug}')" 
+                type="button" class="btn btn-secondary">Show Details</button>
+            
+            
+            `;
+    
+    phoneDetail.appendChild(div);
+}
+const info = name => {
+    const url = `https://openapi.programming-hero.com/api/phone/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => phoneSpecification(data.data));
+}
+
+const phoneSpecification = (datap) =>{
+    // console.log(phoneInfo);
+    const phoneDiv = document.getElementById('phone-info');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    phoneDiv.innerHTML =`
+    <div class="card-body">
+    <h5 class="card-title">Phone Specification</h5>
+          <h5 class="card-title">${datap.releaseDate}</h5>
+              <p>Main Features: ${datap.mainFeatures.storage}</p>
+              <p> Other Specification: ${datap.others.WLAN}</p>
+        <h5 >Release date: ${datap.releaseDate}</h5>`;
+    phoneDiv.appendChild(div);
+}
+
+
+   
+    
